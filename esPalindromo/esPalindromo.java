8425177ff9e226875f443047b01f590925f71ef1
Christian <153014450+CacaEnKilos@users.noreply.github.com>
2024-02-16 17:02:30 +0100

    Update esPalindromo (if para LowNumberException)

--- esPalindromo/esPalindromo.java
+++ esPalindromo/esPalindromo.java
@@ -13,9 +13,12 @@ import com.mycompany.Exceptions.LowNumberException;
  */
 public class esPalindromo {
 
-    public boolean esPalindromo(String palabra) {
+ public boolean esPalindromo(String palabra) throws LowNumberException, DigitException {
         String palabraFormateada = palabra.replaceAll("\\s+", "").toLowerCase();
-        
+        if (palabra.length() <= 1) {
+            throw new LowNumberException("Has de introducir una palabra de menos de 1 letra");
+        } else {
+        }
         for (int i = 0; i < palabraFormateada.length() / 2; i++) {
             if (palabraFormateada.charAt(i) != palabraFormateada.charAt(palabraFormateada.length() - i - 1)) {
                 return false;
