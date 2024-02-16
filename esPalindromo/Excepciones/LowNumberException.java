cf72266c7d9854c8bbeb568e0a8b4a6347bdddb6
Mhernansanz <153014764+Mhernansanz@users.noreply.github.com>
2024-02-16 16:56:57 +0100

    Añado la excepcion LowNumberException

---
+++ esPalindromo/Excepciones/LowNumberException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Exceptions;
+
+/**
+ *
+ * @author dev
+ */
+public class LowNumberException extends Exception{
+    public LowNumberException (String mensaje){
+        super(mensaje);
+    }
+}
