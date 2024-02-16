0f77be2eb7571722291d8050cfa900bd44394c56
NoahSP21 <153014526+NoahSP21@users.noreply.github.com>
2024-02-16 17:11:55 +0100

    Update ContarVocales

--- GestorPalabras/src/test/java/ContarVocalesTest.java
+++ GestorPalabras/src/test/java/ContarVocalesTest.java
@@ -12,21 +12,19 @@ public class ContarVocalesTest {
     public void TestContarConsonantes() {
         GestorPalabras c = new GestorPalabras();
         assertEquals(4,c.contarVocales("ssiss"));
+        assertEquals(4,c.contarVocales("aaayyys"));
+        assertEquals(10,c.contarVocales("pepe tiene un tractor"));
     }
     //tiene que fallar 
     @Test
     public void TestContarVocales() {
         GestorPalabras c = new GestorPalabras();
         assertEquals(1,c.contarVocales("ssiss"));
+        assertEquals(1,c.contarVocales("hay"));
+        assertEquals(8,c.contarVocales("yooo kamekamehahhh"));
+        assertEquals(6,c.contarVocales("allí esta papá"));
+        assertEquals(4,c.contarVocales("pingüino"));        
     }
     //tiene que aceptarlo
-    @Test
-    public void TestAceptaNum() {
-       GestorPalabras c = new GestorPalabras();
-        assertEquals(0,c.contarVocales(p3p3)); 
-    }
-//    public void TestPalabraInvertida() {
-//        GestorPalabras c = new GestorPalabras();
-//        assertEquals("y", c.invertirPalabra("yo"));
-//    }
+
 }
