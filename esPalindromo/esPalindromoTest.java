7a2045d9eef045017341b759974fdcfa93391d89
Mhernansanz <153014764+Mhernansanz@users.noreply.github.com>
2024-02-16 16:49:26 +0100

    Añado el test True

---
+++ esPalindromo/esPalindromoTest.java
@@ -0,0 +1,22 @@
+﻿
+import com.mycompany.maven.GestorPalabras;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
+import org.junit.Test;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author dev
+ */
+public class esPalindromoTest {
+
+    @Test
+    public void esPalindromoTrue()  {
+        GestorPalabras c = new GestorPalabras();
+        assertTrue(c.esPalindromo("madam"));
+    }
+}
