73d26797cf553ce8e2b5c67142173bd6ec3b4727
RickRD14 <153659398+RickRD14@users.noreply.github.com>
2024-02-16 17:14:42 +0100

    Add new PalabraInvertidaTest.java
    Its where it is suppossed to be

---
+++ GestorPalabras/src/test/java/PalabraInvertidaTest.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import com.dam.gestorpalabras.GestorPalabras;
+import static junit.framework.TestCase.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author dev
+ */
+public class PalabraInvertidaTest {
+
+    @Test
+    public void invertirPalabraTest1() {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("hello", gp.invertirPalabra("olleh"));
+    }
+
+    @Test
+    public void invertirPalabraTest2() {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("10loL", gp.invertirPalabra("Lol01"));
+    }
+
+    @Test
+    public void invertirPalabraTest3() {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("sccs olol", gp.invertirPalabra("lolo" + " " + "sccs"));
+    }
+    
+    @Test
+    public void invertirPalabraTest4() {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("ihihih", gp.invertirPalabra("hihihi "));
+    }
+    
+    @Test
+    public void invertirPalabraTest5() {
+        GestorPalabras gp = new GestorPalabras();
+        assertEquals("ihihih", gp.invertirPalabra("hihihi"));
+    }
+}
