40e5a0334e68686de4056b7739edb748e9331b8a
NoahSP21 <153014526+NoahSP21@users.noreply.github.com>
2024-02-20 18:57:25 +0100

    metodo de contar vocales en main arreglado

--- GestorPalabras/src/main/java/com/dam/gestorpalabras/GestorPalabras.java
+++ GestorPalabras/src/main/java/com/dam/gestorpalabras/GestorPalabras.java
@@ -1,17 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.dam.gestorpalabras;
+package GestionPalabras;
 
 /**
  *
  * @author dev
  */
 public class GestorPalabras {
-
     public boolean esPalindromo(String palabra) {
         String palabraFormateada = palabra.replaceAll("\\s+", "").toLowerCase();
-
+        
         for (int i = 0; i < palabraFormateada.length() / 2; i++) {
             if (palabraFormateada.charAt(i) != palabraFormateada.charAt(palabraFormateada.length() - i - 1)) {
                 return false;
@@ -30,7 +30,7 @@ public class GestorPalabras {
         }
         return contador;
     }
-
+// Hay que cambiar el
     public String invertirPalabra(String palabra) {
         StringBuilder resultado = new StringBuilder();
         for (int i = palabra.length() - 2; i >= 0; i--) {
